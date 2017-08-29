b818ff0258b24114ac813d0a37476daba9f66cbd
Sam Fisher <samfisher.1990@yahoo.com>
2017-08-29 14:01:49 +0300

    Depot, Smarter Cart

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -27,12 +27,12 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(:product => product)
+    @line_item = @cart.add_product(product.id)
     session[:counter] = 0
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
@@ -58,9 +58,10 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
+    @cart = current_cart
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.html { redirect_to  @cart, notice: 'Line item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end

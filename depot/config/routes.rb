732df8bd8707b3de89ebbda2d4e0c9807e361e76
Sam Fisher <samfisher.1990@yahoo.com>
2017-10-25 13:43:30 +0300

    Depot, add atom feed for orders

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :orders
+	resources :orders
 	resources :line_items
 	resources :line_items do
 		member do
 			put 'decrement'
 		end
 	end	
+
+	resources :products do
+		get :who_bought, :on => :member
+	end
 
 	resources :carts
 	get 'store/index'
@@ -14,4 +18,4 @@ Rails.application.routes.draw do
   	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   	root :to => 'store#index', :as => 'store'
-end
+  end

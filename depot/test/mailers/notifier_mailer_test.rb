f4fa91bd47e020352c7a772cea9a94c88831e9ab
Sam Fisher <samfisher.1990@yahoo.com>
2017-10-28 10:19:01 +0300

    Depot, Add mailer unit tests

--- depot/test/mailers/notifier_mailer_test.rb
+++ depot/test/mailers/notifier_mailer_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class NotifierMailerTest < ActionMailer::TestCase
   test "order_received" do
-    mail = NotifierMailer.order_received
-    assert_equal "Order received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = NotifierMailer.order_received (orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /This is just to let you know that we've received your recent order/, mail.body.encoded
   end
 
   test "order_shipped" do
-    mail = NotifierMailer.order_shipped
-    assert_equal "Order shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = NotifierMailer.order_shipped (orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /This is just to let you know that we've shipped your recent order/, mail.body.encoded
   end
 
 end
